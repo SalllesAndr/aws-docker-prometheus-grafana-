7705c3db92e4091f40eadf7739b8b3ef1fcb1985
SalllesAndr <salllesandrr@gmail.com>
2025-02-07 17:21:11 -0300

    New code stack-prometheus

--- terraform/scripts/user_data.sh
+++ terraform/scripts/user_data.sh
@@ -4,50 +4,27 @@ set -eux
 echo "Iniciando instalação na instância EC2..."
 
 # Atualiza pacotes e instala dependências essenciais
-sudo apt update -y
-sudo apt install -y ca-certificates curl gnupg git
+sudo yum update -y
+sudo yum install -y docker git vim
 
-echo "Adicionando chave do repositório Docker..."
-# Criando diretório seguro para chave GPG do Docker
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo tee /etc/apt/keyrings/docker.asc > /dev/null
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+# Inicia o serviço Docker
+sudo service docker start
 
-echo "Adicionando repositório oficial do Docker..."
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] \
-  https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# Adiciona o usuário padrão ao grupo Docker
+sudo usermod -aG docker ec2-user
 
-# Atualiza pacotes novamente após adicionar repositório
-sudo apt update -y
+# Configura Docker para iniciar automaticamente
+sudo chkconfig docker on
 
-echo "Instalando Docker e dependências..."
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-echo "Instalando Docker Compose Standalone..."
+# Instala a versão mais recente do Docker Compose Standalone
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-docker-compose version  # Verifica se foi instalado corretamente
 
-echo "Adicionando usuário 'ubuntu' ao grupo Docker..."
-sudo usermod -aG docker ubuntu
-newgrp docker
+# Verifica as versões instaladas
+docker version
+docker-compose version
 
-echo "Habilitando e iniciando serviço Docker..."
-sudo systemctl enable docker
-sudo systemctl start docker
+# Ajusta permissões para evitar problemas com Docker
+sudo chmod 666 /var/run/docker.sock
 
-echo "Clonando o repositório stack-prometheus..."
-cd /home/ubuntu
-git clone https://github.com/SalllesAndr/aws-docker-prometheus-grafana- aws-docker-prometheus-grafana
-
-
-echo "Ajustando permissões..."
-sudo chown -R ubuntu:ubuntu /home/ubuntu/aws-docker-prometheus-grafana
-
-
-echo "Subindo os containers com Docker Compose..."
-cd /home/ubuntu/aws-docker-prometheus-grafana/stack-prometheus
-docker-compose up -d
-
-
-echo "Instalação concluída! Prometheus e Grafana estão rodando."
+echo "Instalação concluída!"
