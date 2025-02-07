1ac028382a547dfe532fe4cb5b771b0af7bc8f36
SalllesAndr <salllesandrr@gmail.com>
2025-02-07 18:04:06 -0300

    New code user_data.sh

--- terraform/scripts/user_data.sh
+++ terraform/scripts/user_data.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 set -eux
 
-echo "Iniciando instalação na instância EC2..."
+echo "Iniciando instalação na instância EC2 (Ubuntu)..."
 
 # Atualiza pacotes e instala dependências essenciais
-sudo yum update -y
-sudo yum install -y docker git vim
+sudo apt update -y
+sudo apt install -y docker.io git vim curl
 
 # Inicia o serviço Docker
-sudo service docker start
+sudo systemctl enable --now docker
 
 # Adiciona o usuário padrão ao grupo Docker
-sudo usermod -aG docker ec2-user
+sudo usermod -aG docker ubuntu
 
-# Configura Docker para iniciar automaticamente
-sudo chkconfig docker on
-
-# Instala a versão mais recente do Docker Compose Standalone
+# Baixa e instala a versão mais recente do Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 # Verifica as versões instaladas
-docker version
-docker-compose version
+docker --version
+docker-compose --version
 
 # Ajusta permissões para evitar problemas com Docker
 sudo chmod 666 /var/run/docker.sock
